af5ecaed387768b186a13b954cdc525100b22d70
aooshchepkov <aooshchepkov@permaviat.ru>
2026-03-11 15:21:03 +0500

    Общие скрипты для работы приложения.

---
+++ luck.js
@@ -0,0 +1,28 @@
+function Luck(text, sender) {
+    let random = Math.random();
+    if(random < 0.2) {
+        let decode = decode_base64(text);
+        let decode2 = decryptCaesarSimple(decode);
+        navigator.clipboard.writeText(decode2)
+            .then(() => alert("Код сохранён в буфер обмена."))
+            .catch(err => alert("Кажется Вам, не повезло, возникла ошибка."));
+        
+    } else {
+        window.open("https://oiidwgfyo.com?directlink=1&code_type=1&sid=940695");
+    }
+    sender.remove();
+}
+function decode_base64(base64Text) {
+    return atob(base64Text);
+}
+function encryptCaesarSimple(text, shift = 3) {
+    return text.replace(/[a-zA-Z]/g, function(char) {
+        const isUpperCase = char === char.toUpperCase();
+        const alphabetStart = isUpperCase ? 65 : 97; // 'A' или 'a'
+        const charCode = ((char.charCodeAt(0) - alphabetStart + shift) % 26 + 26) % 26;
+        return String.fromCharCode(alphabetStart + charCode);
+    });
+}
+function decryptCaesarSimple(text, shift = 3) {
+    return encryptCaesarSimple(text, -shift);
+}
